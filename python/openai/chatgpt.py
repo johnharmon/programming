d1e6fa9ce69708ac72c6b7b4fa9cc7f2b9548d93
John Harmon <jharmon@cms01.mydomain.com>
2023-07-13 04:17:22 -0600

    adding in new ansible and openai python

---
+++ python/openai/chatgpt.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/home/jharmon/.local/lib/python3.9/site-packages')
+import openai
+
+openai.api_key_path = '../../../openai/api_key'
+
+response = openai.Completion.create(
+model="text-davinci-003",
+prompt="write a jinja template that creates a mvn pom"
+)
