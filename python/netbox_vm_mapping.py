ae729f1393c4f90f3c8d85982be6d2b6f7ae18dd
John Harmon <jharmon@cms01.mydomain.com>
2023-10-25 18:40:00 -0600

    adding in netbox_api class

--- python/netbox_vm_mapping.py
+++ python/netbox_vm_mapping.py
@@ -3,6 +3,37 @@
 import requests, json, re, typing
 import vms
 
+class netbox_api():
+    def __init__(self, url, api_token):
+        self.__kind = ''
+        self.__root_url = url 
+        self._url = self.__root_url
+        self.__token = api_token 
+        self.auth = f'Token {self.__token}'
+        self.headers = {'Content-Type': 'application/json', 'Authorization': self.auth}
+    @property
+    def kind(self):
+        return self.__kind 
+    @kind.setter
+    def kind(self, kind)
+        self.__kind = kind 
+        self._url = f'{self.__root_url}/{self.__kind}/'
+    @property 
+    def url(self):
+        return self._url 
+    @url.setter
+    def url(self, url):
+        self.__root_url = url
+    def post(self, data):
+        response = requests.post(url = self.url, headers = self.headers, data = json.dumps(data))
+        return response
+    def put(self, data):
+        response = requests.put(url = self.url, headers = self.headers, data = json.dumps(data))
+        return response
+    def get(self):
+        response = requests.get(url = self.url, headers = self.headers)
+        return response
+
 def create_resource(api, resource_name):
     body = {
         'name': resource_name
@@ -22,7 +53,7 @@ def generate_query_string(**kwargs):
     return query_string
 
 def check_resource_exists(api, resource_name, mode = 'create'):
-    response = requests.get(url = api.url)
+    
     query = generate_query_string(name = resource_name)
     response = requests.get(url = f'{api.url}/{query}', headers = {'Accept': 'application/json', 'Authorization': api.auth})
     if response.status_code != 201:
@@ -55,7 +86,7 @@ def map_values_to_ids(vm_data, api):
             if result:
                 vm_data[key] = result['id']
             else:
-                raise ValueError(f'Resource of type {key} at endpoint {api.kind} must be created manually!')
+                raise ValueError(f'Resource of type {key} at endpoint {api.url} must be created manually!')
 
         elif key == 'tenant':
             api.kind = 'tenancy/tenants'
@@ -63,7 +94,7 @@ def map_values_to_ids(vm_data, api):
             if result:
                 vm_data[key] = result['id']
             else:
-                raise ValueError(f'Resource of type {key} at endpoint {api.kind} must be created manually!')
+                raise ValueError(f'Resource of type {key} at endpoint {api.url} must be created manually!')
         else: 
             pass
     return vm_data
