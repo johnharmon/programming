cd850c50562e7cc518b0ec491d48329cdef09a1b
John Harmon <jharmon@harmonlab.com>
2024-09-20 09:38:21 -0600

    updating go projects

---
+++ go/daily-projects/20240920 - Log Analyzer/analyzer.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func AnalyzeFile(filepath string) (map[string]int, error) {
+	debug := false
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("error opening the file: %w", err)
+	}
+	message_counts := map[string]int{
+		"INFO":  0,
+		"WARN":  0,
+		"ERROR": 0,
+		"FATAL": 0,
+		"DEBUG": 0,
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	if debug {
+		fmt.Printf("Starting to scan %s\n", filepath)
+	}
+
+	for scanner.Scan() {
+
+		if debug {
+			fmt.Println("Scanning line")
+		}
+
+		line := scanner.Bytes()
+		header_end := bytes.Index(line, []byte("\072"))
+		line_start := line[:header_end]
+		line_start_string := string(line_start)
+
+		if debug {
+			fmt.Println(line_start_string)
+		}
+
+		switch {
+		case line_start_string == "INFO":
+			message_counts["INFO"]++
+		case line_start_string == "WARN":
+			message_counts["WARN"]++
+		case line_start_string == "DEBUG":
+			message_counts["DEBUG"]++
+		case line_start_string == "FATAL":
+			message_counts["FATAL"]++
+		case line_start_string == "ERROR":
+			message_counts["ERROR"]++
+		}
+		if err := scanner.Err(); err != nil {
+			return message_counts, err
+		}
+	}
+	return message_counts, nil
+}
+
+func PrintAnalysis(message_counts map[string]int) {
+	keys_alphabetical := make([]string, 0, len(message_counts))
+	for key := range message_counts {
+		keys_alphabetical = append(keys_alphabetical, key)
+	}
+	sort.Strings(keys_alphabetical)
+	for _, key := range keys_alphabetical {
+		fmt.Printf("%s Messages: %d\n", key, message_counts[key])
+	}
+}
+
+func main() {
+	var filepath string
+
+	if len(os.Args) > 1 {
+		filepath = os.Args[1]
+	} else {
+		filepath = "log.txt"
+	}
+	fmt.Printf("Analyzing file: %s\n", filepath)
+	message_counts, err := AnalyzeFile(filepath)
+	if err != nil {
+		fmt.Printf("Error analyzing file: %s\n", err)
+		fmt.Printf("However, before running into an error, here are the counts we got for each message level:\n")
+		PrintAnalysis(message_counts)
+		/*
+			keys_alphabetical := make([]string, 0, len(message_counts))
+			for key, _ := range message_counts {
+				keys_alphabetical = append(keys_alphabetical, key)
+			}
+			sort.Strings(keys_alphabetical)
+			for _, key := range keys_alphabetical {
+				fmt.Printf("%s Messages: %d", key, message_counts[key])
+			}
+		*/
+	} else {
+		PrintAnalysis(message_counts)
+	}
+}
---
+++ go/linux-commands/git/src/output.txt
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+type GitTree struct {
+	entryCount int
+	entries    []GitTreeEntry
+	hash       []byte
+	name       string
+	size       int
+}
+
+type FileDetails struct {
+	Name      string
+	Path      string
+	Size      int64
+	Mode      os.FileMode
+	ModTime   string
+	IsDir     bool
+	IsSymlink bool
+	Stat      *syscall.Stat_t
+}
+
+type GitObject interface {
+	GetType() string
+	GetHash() []byte
+	GenHash() []byte
+}
+
+type GitBlob struct {
+	hash    []byte
+	size    int64
+	name    string
+	details *FileDetails
+	content *[]byte
+}
+
+type GitCommit struct {
+	header        string
+	hash          []byte
+	treeHash      []byte
+	parentCount   int
+	parentHashes  *[][]byte
+	authorName    string
+	authorEmail   string
+	authorTime    time.Time
+	comitterName  string
+	comitterEmail string
+	comitterTime  time.Time
+	message       string
+}
+
+type GitTreeEntry struct {
+	mode      int
+	entryType string
+	hash      []byte
+	name      string
+	object    GitObject
+}
+
+type IpHeader struct {
+	version        [4]byte
+	headerLength   [3]byte
+	typeOfService  [8]byte
+	totalSize      [16]byte
+	identification [16]byte
+	flags          [3]byte
+	fragmentOffset [13]byte
+	timeToLive     [8]byte
+	protocol       [8]byte
+	headerChecksum [16]byte
+	sourceIp       [32]byte
+	destinationIp  [32]byte
+	options        [32]byte
+}
