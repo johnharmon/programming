cd0e5029703d1f2a4b739151343497a4e2a0d9a3
John Harmon <jharmon@harmonlab.com>
2024-09-22 13:48:46 -0600

    updating linkedList.go

--- go/daily-projects/20240922-Linked-List/linkedList.go
+++ go/daily-projects/20240922-Linked-List/linkedList.go
@@ -1,22 +1,28 @@
-package main 
+package main
 
+import "fmt"
 
 type listNode struct {
-	data int 
-	next *listNode 
+	data int
+	next *listNode
 }
 
 type linkedList struct {
 	head *listNode
 }
 
-
 func findListEnd(list *linkedList) (previousNode *listNode, lastNode *listNode) {
+	previousNode = nil
+	//	fmt.Printf("finding the end of the list\n")
+	//	fmt.Printf("List is %+v\n", list)
 	head := list.head
-	var previousNode *listNode
+	//fmt.Printf("Head is: %+v\n", head)
+	//fmt.Printf("head found\n")
 	for {
-		if !head.next {
-			return previousNode, lastNode := head
+		//fmt.Printf("Loop entered\n")
+		if head.next == nil {
+			lastNode = head
+			return previousNode, lastNode
 		} else {
 			previousNode = head
 			head = head.next
@@ -24,87 +30,173 @@ func findListEnd(list *linkedList) (previousNode *listNode, lastNode *listNode) 
 	}
 }
 
+func findValue(list *linkedList, value int) *listNode {
+	head := list.head
+	for {
+		if head.data == value {
+			return head
+		} else {
+			if head.next != nil {
+				head = head.next
+			} else {
+				return nil
+			}
+		}
+	}
+}
 
-func removeFromEnd(list *linkedList){
-	newTailNode, oldTailNode := findListEnd(list)
+func removeFromEnd(list *linkedList) {
+	newTailNode, _ := findListEnd(list)
 	if newTailNode == nil { // Single node list
 		list.head = nil
 	} else {
-	newTailNode.next = nil 
+		newTailNode.next = nil
 	}
 	/*
-	var previousNode listNode 
-	index := 0
-	for {
-		if head.next {
-			previousNode = head 
-			head = head.next
-			index++
-		} else {
-			if index == 0 {
-				head = nil
+		var previousNode listNode
+		index := 0
+		for {
+			if head.next {
+				previousNode = head
+				head = head.next
+				index++
 			} else {
-				head = nil 
-				previousNode.next = nil
+				if index == 0 {
+					head = nil
+				} else {
+					head = nil
+					previousNode.next = nil
+				}
+				break
 			}
-			break
 		}
-	}
 	*/
 }
 
-func removeNode(head *listnode, previousNode *listNode) {
+func removeNodePtr(head **listNode, previousNode *listNode) {
 	if previousNode != nil { // We are not at the head of the list
-		if head.next { // we are not at the end either
-			previousNode.next = head.next // Update previous node pointer
-			head = nil
+		if (*head).next != nil { // we are not at the end either
+			previousNode.next = (*head).next
+			*head = nil
 		} else { // We are at the end of the list
-			head = nil // Delt last node
-			previousNode.next = nil // Zero out pointer for the next node on the new tail
+			*head = nil // Delt last node
 		}
 	} else { // We are at the current head of the list
-		if head.next {
-			head = head.next // Move head to the second element, GC will clean up the old head
+		if (*head).next != nil {
+			*head = (*head).next // Move head to the second element, GC will clean up the old head
 		} else { // We are a one node list
-			head = nil // Zero out only existing node
+			*head = nil // Zero out only existing node
+		}
+	}
+}
+
+func removeNode(head *listNode, previousNode *listNode) {
+	if previousNode != nil { // We are not at the head of the list
+		if head.next != nil { // we are not at the end either
+			previousNode.next = head.next
+			head = nil
+		} else { // We are at the end of the list
+			fmt.Println("removing last node")
+			previousNode.next = nil
 		}
 	}
 }
 
 func removeNodeValue(list *linkedList, value int) bool {
-	var previousNode *listNode 
+	var previousNode *listNode
 	head := list.head
-	index := 0
-	for {
-		if head.data == value {
-			removeNode(head, previousNode)
+	if list.head.data == value {
+		if list.head.next != nil {
+			list.head = list.head.next
+			return true
+		} else {
+			list.head = nil
 			return true
 		}
-		if head.next {
-			head = head.next 
-		} else {
-			return false
+	} else {
+		//index := 0
+		for {
+			if head.data == value {
+				removeNode(head, previousNode)
+				return true
+			}
+			if head.next != nil {
+				previousNode = head
+				head = head.next
+			} else {
+				return false
+			}
 		}
 	}
 }
 
-func insertAtBeginning (list *linkedList, value int) {
-	newHead := &listNode{ 
-		next: listNode.next 
-		data: int  
+func insertAtBeginning(list *linkedList, value int) {
+	newHead := &listNode{
+		next: list.head,
+		data: value,
 	}
-	list.next = newHead
+	list.head = newHead
 }
 
 func insertAtEnd(list *linkedList, value int) {
+	//fmt.Printf("Adding %d to list\n", value)
 	_, listEnd := findListEnd(list)
 	newEnd := &listNode{
-		data: value 
-		next: nil
+		data: value,
+		next: nil,
 	}
 	listEnd.next = newEnd
 }
 
-func main () {
+func printList(list *linkedList) {
+	head := list.head
+	index := 0
+	if head != nil {
+		for {
+			if head != nil {
+				fmt.Printf("Linked List as position %d has the value of %+v\n", index, head)
+				index++
+				head = head.next
+			} else {
+				return
+			}
+		}
+	}
+}
+
+func main() {
+
+	// make first node
+	firstNode := &listNode{
+		data: 5,
+		next: nil,
+	}
+
+	// make list struct
+	myList := linkedList{
+		head: firstNode,
+	}
+
+	// populate list
+	for i := 0; i < 10; i++ {
+		//fmt.Printf("Adding to linked list\n")
+		insertAtEnd(&myList, i)
+	}
+
+	// display list after population
+	//printList(&myList)
+
+	// add to beginning of list
+	insertAtBeginning(&myList, 100)
+
+	// display list after insertion at the beginning
+	//printList(&myList)
+
+	// remove value from list
+	removeNodeValue(&myList, 9)
+	//fmt.Printf("Value was removed: %t\n", removed)
+
+	// Print list after value removal
+	printList(&myList)
 
 }
