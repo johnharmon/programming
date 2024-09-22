eb48e6d9209e3e54702fbf179ac76e485cb7c4b3
John Harmon <jharmon@harmonlab.com>
2024-09-22 11:47:17 -0600

    updating with linked list project from chat gpt

--- go/daily-projects/20240921-Channel-Practice-Adv/channelsAdv.go
+++ go/daily-projects/20240921-Channel-Practice-Adv/channelsAdv.go
@@ -22,6 +22,7 @@ func genTasks(limit int, taskWg *sync.WaitGroup) []chan int {
 	}
 	return taskChannels
 }
+
 func main() {
 	limit := 20000
 	taskWg := &sync.WaitGroup{}
@@ -52,9 +53,7 @@ func main() {
 					return
 				default:
 				}
-
 			}
-
 		}(itr)
 	}
 	workerWg.Wait()
