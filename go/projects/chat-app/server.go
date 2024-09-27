e5bc1d3718b56c7eb168647805bd8c046856d081
John Harmon <jharmon@harmonlab.com>
2024-09-27 16:36:01 -0600

    adding in start of chat application:)

---
+++ go/projects/chat-app/server.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+)
+
+type ChatMessage struct { // All messages will be wrapped/unwrapped via this struct on either side
+	Username     string `json:"username"`
+	Message      string `json:"message"`
+	Timestamp    int64  `json:"timestamp"`
+	ConnectionID int    `json:"connection_id"`
+}
+
+type ChatConnectionState struct { // Will be used to update the server that connections are being made/terminated
+	Username     string `json:"username"`
+	State        string `json:"connection_state"`
+	ConnectionID int    `json:"connection_id"`
+}
+
+type ChatClient struct {
+	Username          string
+	ConnectionAddress net.IP
+	ConnectionChannel chan ChatMessage
+	ConnectionID      int
+}
+
+type ClientUpdate struct {
+	ClientID     string
+	ConnectionID int
+	Channel      chan ChatMessage
+	Action       string // "connect" or "disconnect"
+}
+
+func NewClientUpdate(clientID string, connectionID int, chatChan chan ChatMessage, action string) ClientUpdate {
+
+}
+
+type GlobalBroadcaster struct {
+	globalProducer chan ChatMessage
+	//globalConsumers map[string]chan ChatMessage // map of all channels clients are consuming from, whenever anything is sent to the global producer, it replicates to all global consumers
+	ClientUpdates chan ClientUpdate  // channel that takes connection states, used to update the internal map of client channeld to send to
+	globalClients map[int]ChatClient // map of chat clients, try to coordinate ChatClient.ConnectionID with the map key
+}
+
+func (gb *GlobalBroadcaster) addConnection(conn *net.Conn) error {
+	return nil
+}
+
+func (gb *GlobalBroadcaster) Broadcast(cm ChatMessage) {
+	for connID, chatClient := range gb.globalClients {
+		if connID != cm.ConnectionID {
+			chatClient.ConnectionChannel <- cm
+		}
+	}
+}
+
+func (gb *GlobalBroadcaster) RemoveClient(cu ClientUpdate) bool {
+	for k, v := range gb.globalClients {
+		if v.ConnectionID == cu.ConnectionID {
+			delete(gb.globalClients, k)
+			return true
+		}
+	}
+	return false
+}
+func NewChatClient(cu ClientUpdate) ChatClient {
+	cc := ChatClient{
+		Username:          cu.ClientID,
+		ConnectionAddress: nil,
+		ConnectionChannel: make(chan ChatMessage),
+		ConnectionID:      cu.ConnectionID,
+	}
+	return cc
+}
+
+func (gb *GlobalBroadcaster) UpdateClient(cu ClientUpdate) bool {
+	if cu.Action == "connect" {
+		//gb.globalClients[cu.ConnectionID] = NewChatClient(cu)
+		gb.globalClients[cu.ConnectionID] = NewChatClient(cu)
+		return true
+	} else if cu.Action == "disconnect" {
+		gb.RemoveClient(cu)
+		return false
+	}
+	return false
+}
+
+func ManageGlobalBroadcaster(gb *GlobalBroadcaster) {
+	for {
+		select {
+		case message := <-gb.globalProducer:
+			go gb.Broadcast(message)
+			//write message to all consumers
+		case clientUpdate := <-gb.ClientUpdates:
+			gb.UpdateClient(clientUpdate)
+			// method for updating client map, closing/opening channels, etc
+		}
+	}
+}
+
+func handleConnection(conn net.Conn, gb *GlobalBroadcaster, lci *int) error {
+	clientUpdate := ClientUpdate{}
+	dataBuf := []byte{}
+	_, err := conn.Read(dataBuf)
+	if err != nil {
+		return fmt.Errorf("error reading from connnection: %w", err)
+	}
+	uErr := json.Unmarshal(dataBuf, &clientUpdate)
+	if uErr != nil {
+		return fmt.Errorf("error unmarshalling data into ChatClient instance: %w", uErr)
+	}
+	gb.ClientUpdates <- clientUpdate
+
+	return nil
+
+}
+
+func main() {
+
+	// Listen on TCP port 8000 on all interfaces.
+	ActiveConnections := []*ChatClient{}
+	if len(ActiveConnections) < 1 {
+		fmt.Printf("\r")
+	}
+
+	lastConnectionID := 0
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	broadcaster := GlobalBroadcaster{}
+
+	fmt.Println("Chat server started on port 8000")
+
+	// Accept new connections in a loop.
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		// Handle each connection in a new goroutine.
+		go handleConnection(conn, &broadcaster, &lastConnectionID)
+	}
+}
