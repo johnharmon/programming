8b7ab06ca47126430a5fa331c187437c9a3ea891
John Harmon <placeholder>
2025-02-01 16:54:57 -0700

    updating kubernetes webapp and adding subnet ping utility

--- kubernetes/go-webapp/src/main.go
+++ kubernetes/go-webapp/src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -11,18 +10,14 @@ func testResponse(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	secret, err := generateJWTSecret()
-	if err != nil {
-		log.Fatalf("Error generating secret:\n\t%v", err)
-	}
 	http.HandleFunc("/test", testResponse)
-	http.HandleFunc("/jwt/token/get", CreateWebTokenHandler(secret))
-	http.HandleFunc("/jwt/token/validate", ValidateWebTokenHandlerDebugger(secret))
-	http.HandleFunc("/jwt/token/protected", TokenValidationMiddleware(http.HandlerFunc(ProtectedRouteHandler), secret, "set_cookie"))
-	http.HandleFunc("/jwt/token/keymap", TokenValidationMiddleware(http.HandlerFunc(DisplayKeyMapHandler), secret, "set_cookie"))
+	http.HandleFunc("/jwt/token/get", CreateWebTokenHandler())
+	http.HandleFunc("/jwt/token/validate", ValidateWebTokenHandlerDebugger())
+	http.HandleFunc("/jwt/token/keymap", TokenValidationMiddleware(http.HandlerFunc(DisplayKeyMapHandler), "set_cookie"))
 	http.HandleFunc("/index", ServeEmbeddedStaticContent(files, "files/index.html"))
-	http.Handle("/jwt/token/protected2", TokenValidationMiddlewareHandler(http.HandlerFunc(ProtectedRouteHandler), secret, "set_cookie"))
-	fmt.Printf("Starting server on port 8080")
+	http.HandleFunc("/jwt/token/protected", TokenValidationMiddleware(http.HandlerFunc(ProtectedRouteHandler), "set_cookie"))
+	http.Handle("/jwt/token/protected2", TokenValidationMiddlewareHandler(http.HandlerFunc(ProtectedRouteHandler), "set_cookie"))
+	fmt.Printf("Starting server on port 8080\n")
 	serveErr := http.ListenAndServe(":8080", nil)
 	if serveErr != nil {
 		fmt.Printf("Error starting server: %v", serveErr)
