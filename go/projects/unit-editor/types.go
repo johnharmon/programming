9915284fb6ab5c482a059686a0fa0feefc4e0332
John Harmon <jharmon@harmonlab.com>
2025-04-01 22:16:40 -0600

    updating with proper pointer modification, added json field tags

--- go/projects/unit-editor/types.go
+++ go/projects/unit-editor/types.go
@@ -179,6 +179,7 @@ func ParseLineRecord(lr *LineRecord) (err error) {
 }
 
 func UnmarshalLineRecord(line string, lineNumber int, unit *Unit) (lr *LineRecord) {
+	lr = &LineRecord{}
 	lr.LineNumber = lineNumber
 	lr.Raw = line
 	lr.Unit = unit
@@ -187,7 +188,6 @@ func UnmarshalLineRecord(line string, lineNumber int, unit *Unit) (lr *LineRecor
 
 	}
 	return lr
-
 }
 
 func (lr *LineRecord) Unmarshal(line string, lineNumber int) {
@@ -209,23 +209,23 @@ type UnitLog struct {
 }
 
 type UnitAttributes struct {
-	SeaFaring          string `unit:"sea_faring"`           // can board ships;can_swim : can swim across rivers
-	HideForest         string `unit:"hide_forest"`          // defines where the unit can hide
-	HideImprovedForest string `unit:"hide_improved_forest"` // defines where the unit can hide
-	HideAnywhere       string `unit:"hide_anywhere"`        // defines where the unit can hide
-	CanSap             string `unit:"can_sap"`              // Can dig tunnels under walls
-	FrightenFoot       string `unit:"frighten_foot"`        // Cause fear to certain nearby unit types
-	FrightenMounted    string `unit:"frighten_mounted"`     // Cause fear to certain nearby unit types
-	CanRunAmok         string `unit:"can_run_amok"`         // Unit may go out of control when riders lose control of animals
-	GeneralUnit        string `unit:"general_unit"`         // The unit can be used for a named character's bodyguard
-	CantabrianCircle   string `unit:"cantabrian_circle"`    // The unit has this special ability
-	NoCustom           string `unit:"no_custom"`            // The unit may not be selected in custom battles
-	Command            string `unit:"command"`              // The unit carries a legionary eagle, and gives bonuses to nearby units
-	MercenaryUnit      string `unit:"mercenary_unit"`       // The unit is s mercenary unit available to all factions
-	IsPeasant          string `unit:"is_peasant"`           // unknown
-	Druid              string `unit:"druid"`                // Can do a special morale raising chant
-	PowerCharge        string `unit:"power_charge"`         // unkown
-	FreeUpkeepUnit     string `unit:"free_upkeep_unit"`     // Unit can be supported free in a city
+	SeaFaring          string `unit:"sea_faring" json:"sea_faring"`                     // can board ships;can_swim : can swim across rivers
+	HideForest         string `unit:"hide_forest" json:"hide_forest"`                   // defines where the unit can hide
+	HideImprovedForest string `unit:"hide_improved_forest" json:"hide_improved_forest"` // defines where the unit can hide
+	HideAnywhere       string `unit:"hide_anywhere" json:"hide_anywhere"`               // defines where the unit can hide
+	CanSap             string `unit:"can_sap" json:"can_sap"`                           // Can dig tunnels under walls
+	FrightenFoot       string `unit:"frighten_foot" json:"frighten_foot"`               // Cause fear to certain nearby unit types
+	FrightenMounted    string `unit:"frighten_mounted" json:"frighten_mounted"`         // Cause fear to certain nearby unit types
+	CanRunAmok         string `unit:"can_run_amok" json:"can_run_amok"`                 // Unit may go out of control when riders lose control of animals
+	GeneralUnit        string `unit:"general_unit" json:"general_unit"`                 // The unit can be used for a named character's bodyguard
+	CantabrianCircle   string `unit:"cantabrian_circle" json:"cantabrian_circle"`       // The unit has this special ability
+	NoCustom           string `unit:"no_custom" json:"no_custom"`                       // The unit may not be selected in custom battles
+	Command            string `unit:"command" json:"command"`                           // The unit carries a legionary eagle, and gives bonuses to nearby units
+	MercenaryUnit      string `unit:"mercenary_unit" json:"mercenary_unit"`             // The unit is s mercenary unit available to all factions
+	IsPeasant          string `unit:"is_peasant" json:"is_peasant"`                     // unknown
+	Druid              string `unit:"druid" json:"druid"`                               // Can do a special morale raising chant
+	PowerCharge        string `unit:"power_charge" json:"power_charge"`                 // unkown
+	FreeUpkeepUnit     string `unit:"free_upkeep_unit" json:"free_upkeep_unit"`         // Unit can be supported free in a city
 
 }
 
@@ -238,36 +238,36 @@ type Unit struct {
 	Logger                 *UnitLogger
 	LineRecords            []*LineRecord
 	Lines                  []string
-	Type                   string            `unit:"type"`
-	Dictionary             string            `unit:"dictionary"`
-	Class                  string            `unit:"class"`
-	VoiceType              string            `unit:"voice_type"`
-	Accent                 string            `unit:"accent"`
-	BannerFaction          string            `unit:"banner_faction"`
-	BannerHoly             string            `unit:"banner_holy"`
-	Soldier                *Soldier          `unit:"soldier"`
-	Officer                string            `unit:"officer"`
-	MountEffect            *MountEffect      `unit:"mount_effect"`
-	Attributes             []string          `unit:"attributes"`
-	Formation              *Formation        `unit:"formation"`
-	StatHealth             *Health           `unit:"stat_health"`
-	StatPrimary            *Weapon           `unit:"stat_pri"`
-	StatPrimaryAttribute   *WeaponAttributes `unit:"stat_pri_attr"`
-	StatSecondary          *Weapon           `unit:"stat_sec"`
-	StatSecondaryAttribute *WeaponAttributes `unit:"stat_sec_attr"`
-	StatPrimaryArmor       *Armor            `unit:"Stat_pri_armor"`
-	StatSecondaryArmor     *Armor            `unit:"Stat_sec_armor"`
-	StatHeat               *Heat             `unit:"stat_heat"`
-	StatGround             *Ground           `unit:"stat_ground"`
-	StatMental             string            `unit:"stat_mental"`
-	StatChargeDistance     int               `unit:"stat_charge_dist"`
-	StatFireDelay          int               `unit:"stat_fire_delay"`
-	StatFood               string            `unit:"stat_food"`
-	StatCost               string            `unit:"stat_cost"`
-	ArmorUpgradeLevels     []int             `unit:"armor_upgrade_levels"`
-	ArmorUpgradeModels     []string          `unit:"armor_upgrade_models"`
-	Ownership              string            `unit:"ownership"`
-	RecruitPriorityOffset  int               `unit:"recruit_priority_offset"`
+	Type                   string            `unit:"type" json:"type"`
+	Dictionary             string            `unit:"dictionary" json:"dictionary"`
+	Class                  string            `unit:"class" json:"class"`
+	VoiceType              string            `unit:"voice_type" json:"voice_type"`
+	Accent                 string            `unit:"accent" json:"accent"`
+	BannerFaction          string            `unit:"banner_faction" json:"banner_faction"`
+	BannerHoly             string            `unit:"banner_holy" json:"banner_holy"`
+	Soldier                *Soldier          `unit:"soldier" json:"soldier"`
+	Officer                string            `unit:"officer" json:"officer"`
+	MountEffect            *MountEffect      `unit:"mount_effect" json:"mount_effect"`
+	Attributes             []string          `unit:"attributes" json:"attributes"`
+	Formation              *Formation        `unit:"formation" json:"formation"`
+	StatHealth             *Health           `unit:"stat_health" json:"stat_health"`
+	StatPrimary            *Weapon           `unit:"stat_pri" json:"stat_pri"`
+	StatPrimaryAttribute   *WeaponAttributes `unit:"stat_pri_attr" json:"stat_pri_attr"`
+	StatSecondary          *Weapon           `unit:"stat_sec" json:"stat_sec"`
+	StatSecondaryAttribute *WeaponAttributes `unit:"stat_sec_attr" json:"stat_sec_attr"`
+	StatPrimaryArmor       *Armor            `unit:"Stat_pri_armor" json:"Stat_pri_armor"`
+	StatSecondaryArmor     *Armor            `unit:"Stat_sec_armor" json:"Stat_sec_armor"`
+	StatHeat               *Heat             `unit:"stat_heat" json:"stat_heat"`
+	StatGround             *Ground           `unit:"stat_ground" json:"stat_ground"`
+	StatMental             string            `unit:"stat_mental" json:"stat_mental"`
+	StatChargeDistance     int               `unit:"stat_charge_dist" json:"stat_charge_dist"`
+	StatFireDelay          int               `unit:"stat_fire_delay" json:"stat_fire_delay"`
+	StatFood               string            `unit:"stat_food" json:"stat_food"`
+	StatCost               string            `unit:"stat_cost" json:"stat_cost"`
+	ArmorUpgradeLevels     []int             `unit:"armor_upgrade_levels" json:"armor_upgrade_levels"`
+	ArmorUpgradeModels     []string          `unit:"armor_upgrade_models" json:"armor_upgrade_models"`
+	Ownership              string            `unit:"ownership" json:"ownership"`
+	RecruitPriorityOffset  int               `unit:"recruit_priority_offset" json:"recruit_priority_offset"`
 }
 
 type Soldier struct {
@@ -279,16 +279,16 @@ type Soldier struct {
 
 type MountEffect struct {
 	Effects            map[string]int
-	Horse              int `unit:"horse"`
-	Camel              int `unit:"camel"`
-	Elephant           int `unit:"elephant"`
-	ElephantCannon     int `unit:"elephant_cannon"`
-	SimpleHorse        int `unit:"simple horse"`
-	MountLightWolf     int `unit:"mount_light_wolf"`
-	WargCamel          int `unit:"warg_camel"`
-	SwanGuardHorse     int `unit:"swan guard horse"`
-	Eorlingas          int `unit:"eorlingas"`
-	NorthernHeavyHorse int `unit:"northern heavy horse"`
+	Horse              int `unit:"horse json:"horse"`
+	Camel              int `unit:"camel" json:"camel"`
+	Elephant           int `unit:"elephant" json:"elephant"`
+	ElephantCannon     int `unit:"elephant_cannon" json:"elephant_cannon"`
+	SimpleHorse        int `unit:"simple horse" json:"simple horse"`
+	MountLightWolf     int `unit:"mount_light_wolf" json:"mount_light_wolf"`
+	WargCamel          int `unit:"warg_camel" json:"warg_camel"`
+	SwanGuardHorse     int `unit:"swan guard horse" json:"swan guard horse"`
+	Eorlingas          int `unit:"eorlingas" json:"eorlingas"`
+	NorthernHeavyHorse int `unit:"northern heavy horse" json:"northern heavy horse"`
 }
 
 func (me *MountEffect) Unmarshal(effectInfo string) error {
@@ -306,12 +306,12 @@ func (me *MountEffect) Unmarshal(effectInfo string) error {
 
 // type Attributes struct{}
 type Formation struct {
-	SidetoSideSpacingTight  float64
-	FronttoBackSpacingTight float64
-	SidetoSideSpacingLoose  float64
-	FronttoBackSpacingLoose float64
-	DefaultRanks            int
-	PossibleFormations      []string
+	SidetoSideSpacingTight  float64  `json:"sideTight"`
+	FronttoBackSpacingTight float64  `json:"frontTight"`
+	SidetoSideSpacingLoose  float64  `json:"sideLoose"`
+	FronttoBackSpacingLoose float64  `json:"frontLoose"`
+	DefaultRanks            int      `json:"defaultRanks"`
+	PossibleFormations      []string `json:"possibleFormations"`
 }
 
 func (f *Formation) Unmarshal(formationInfo string, ul *UnitLogger, lr *LineRecord) (fieldErrors error) {
@@ -440,17 +440,17 @@ func (w *Weapon) Unmarshal(weaponInfo string, ul *UnitLogger, lr *LineRecord) (f
 
 type WeaponAttributes struct {
 	Attributes map[string]bool
-	AP         BoolAttribute `unit:"ap"`            // armour piercing. Only counts half of target's armour
-	BP         BoolAttribute `unit:"bp"`            // body piercing. Missile can pass through men and hit those behind
-	Spear      BoolAttribute `unit:"spear"`         // Used for long spears. Gives bonuses fighting cavalry, and penalties against infantry
-	LongPike   BoolAttribute `unit:"long_pike"`     // Use very long pikes. Phalanx capable units only
-	ShortPike  BoolAttribute `unit:"short_pike"`    // Use shorter than normal spears.
-	Prec       BoolAttribute `unit:"prec"`          // Missile weapon is only thrown/ fired just before charging into combat
-	Thrown     BoolAttribute `unit:"thrown"`        // The missile type if thrown rather than fired
-	launching  BoolAttribute `unit:"launching"`     // attack may throw target men into the air
-	Area       BoolAttribute `unit:"area"`          // attack affects an area, not just one man
-	LightSpear BoolAttribute `unit:"light_spear"`   // The unit when braced has various protecting mechanisms versus cavalry charges from the frontk
-	SpearBonus BoolAttribute `unit:"spear_bonus_x"` // attack bonus against cavalry. x = 2, 4, 6, 8, 10 or 12
+	AP         BoolAttribute `unit:"ap" json:"ap"`                       // armour piercing. Only counts half of target's armour
+	BP         BoolAttribute `unit:"bp" json:"bp"`                       // body piercing. Missile can pass through men and hit those behind
+	Spear      BoolAttribute `unit:"spear" json:"spear"`                 // Used for long spears. Gives bonuses fighting cavalry, and penalties against infantry
+	LongPike   BoolAttribute `unit:"long_pike" json:"long_pike"`         // Use very long pikes. Phalanx capable units only
+	ShortPike  BoolAttribute `unit:"short_pike" json:"short_pike"`       // Use shorter than normal spears.
+	Prec       BoolAttribute `unit:"prec" json:"prec"`                   // Missile weapon is only thrown/ fired just before charging into combat
+	Thrown     BoolAttribute `unit:"thrown" json:"thrown"`               // The missile type if thrown rather than fired
+	Launching  BoolAttribute `unit:"launching" json:"launching"`         // attack may throw target men into the air
+	Area       BoolAttribute `unit:"area" json:"area"`                   // attack affects an area, not just one man
+	LightSpear BoolAttribute `unit:"light_spear" json:"light_spear"`     // The unit when braced has various protecting mechanisms versus cavalry charges from the frontk
+	SpearBonus BoolAttribute `unit:"spear_bonus_x" json:"spear_bonus_x"` // attack bonus against cavalry. x = 2, 4, 6, 8, 10 or 12
 }
 
 type Armor struct {
