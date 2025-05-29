a1145ea871473500095e0ce8a3e0df018840ed0d
John Harmon <placeholder>
2025-05-28 23:07:27 -0400

    starting logger work and entrypoint for window implementation debug

---
+++ go/projects/chat-app/v2/TermReader/types.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+type EphemeralLogger interface {
+	Log(string, ...any)
+	Logln(string, ...any)
+}
+
+type ConcreteLogger struct {
+	Out   io.Writer
+	LogCh chan string
+	RunCh chan interface{}
+}
+
+type KeyAction struct {
+	Children   map[byte]*KeyAction
+	Value      []byte
+	IsTerminal bool
+	Action     string
+	PrintRaw   bool
+	FromPool   bool
+}
+
+type State struct {
+	Windows      []*Window
+	ActiveWindow *Window
+}
+
+type MainConfig struct {
+	LogConfig *LogConfig
+	In        io.Reader
+	Out       io.Writer
+	State     State
+}
+
+type Window struct { // Represents a sliding into its backing buffer of Window.Buf as well as the space it takes up in the terminal window
+	TermTopLine       int
+	BufTopLine        int
+	StartIndex        int
+	StartLine         int
+	Buf               *DisplayBuffer
+	Height            int
+	Width             int
+	StartCol          int
+	CursorLine        int
+	CursorCol         int
+	EndIndex          int
+	RawStartIndex     int
+	RawEndIndex       int
+	Out               io.Writer
+	EventChan         chan *KeyAction
+	RawEventChan      chan []byte
+	KeyActionReturner chan *KeyAction
+}
+
+type LogConfig struct {
+	File *os.File
+	Link *os.File
+}
+
+type PooledKeyAction struct {
+	KA       *KeyAction
+	FromPool bool
+}
+
+type CellHistory struct {
+	RawContent     []byte
+	DisplayContent []byte
+	CursorPosition int
+}
+
+type ModificationSequence struct {
+	Bytes       []byte
+	Name        string
+	Raw         string
+	ForceRedraw bool
+	IsMultiByte bool
+}
+
+type DisplayWrapper struct {
+	TopPattern    string
+	BottomPattern string
+	LinePrefix    string
+	LineSuffix    string
+}
+
+type Env struct {
+	Config       *FlagConfig
+	DebugWriter  io.Writer
+	OutputHeader string
+	OutputFooter string
+	OutputPrefix string
+	OutputSuffix string
+	Logger       EphemeralLogger
+}
+type DisplayBuffer struct { // This represents the full backing buffer to any window view
+	RawBuf         []byte
+	Lines          [][]byte
+	DisplayedLines [][]byte
+	Size           int
+	ActiveLine     int
+	TopLine        int
+	Height         int
+}
+
+type InputScanner struct {
+	Remaining   []byte
+	LastMessage []byte
+	LastChunk   []byte
+	Input       io.Reader
+	Output      io.Writer
+	Delimiter   byte
+}
+
+type FlagConfig struct {
+	Debug      bool
+	Verbose    bool
+	Terminal   bool
+	Verbosity1 bool
+	Verbosity2 bool
+	Verbosity3 bool
+	Verbosity4 bool
+	Window     bool
+	Raw        bool
+	Logs       io.Writer
+}
+
+type FormatInfo struct {
+	OutputRaw      []byte
+	OutputLines    [][]byte // raw bytes representing the output
+	WrappingLength int      // how long the output prefix and suffix combined is
+	TermWidth      int      // how many characters wide the terminal output is
+	TermLength     int      // how many lines long including headers and footers the ouput is
+}
+
+type VirtualBuffer struct {
+	Buf [][]byte
+}
+
+type Cell struct {
+	formatInfo            *FormatInfo
+	RawContent            *bytes.Buffer
+	RawInput              *bytes.Buffer
+	ContentReader         *bytes.Reader
+	Out                   io.Writer
+	VBuf                  [][]byte
+	In                    io.Reader
+	DisplayContent        *bytes.Buffer
+	CursorPosition        int
+	LogicalCursorPosition int
+	DisplayCursorPosition int
+	CursorLine            int
+	CursorColumn          int
+	DisplayBuffer         *DisplayBuffer
+	Window                *Window
+	VirtualBuffer         [][]byte
+	DebugInfo             []string
+	CellHistory           []*Cell
+	ActiveLineIdx         int
+	ActiveLineLength      int
+	LogicalRowIdx         int
+	EffecitveRowIdx       int
+	LogCh                 chan string
+	BufferLen             int
+	Logger                io.Writer
+	LogFile               *os.File
+	LogLink               string
+}
--- go/projects/chat-app/v2/TermReader/window.go
+++ go/projects/chat-app/v2/TermReader/window.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"flag"
 	"fmt"
 	"io"
 	"os"
-	"reflect"
-	"strings"
 	"sync"
 	"time"
 
@@ -19,36 +15,6 @@ var (
 	TERM_CLEAR_SCREEN = []byte{0x1b, '[', '2', 'J'}
 )
 
-type Window struct { // Represents a sliding into its backing buffer of Window.Buf as well as the space it takes up in the terminal window
-	TermTopLine       int
-	BufTopLine        int
-	StartIndex        int
-	StartLine         int
-	Buf               *DisplayBuffer
-	Height            int
-	Width             int
-	StartCol          int
-	CursorLine        int
-	CursorCol         int
-	EndIndex          int
-	RawStartIndex     int
-	RawEndIndex       int
-	Out               io.Writer
-	EventChan         chan *KeyAction
-	RawEventChan      chan []byte
-	KeyActionReturner chan *KeyAction
-}
-
-type LogConfig struct {
-	File *os.File
-	Link *os.File
-}
-
-type PooledKeyAction struct {
-	KA       *KeyAction
-	FromPool bool
-}
-
 func (w Window) Size() int {
 	return w.Height
 }
@@ -57,30 +23,27 @@ func (w Window) GetCursorScreenLine() int {
 	return w.CursorLine - w.BufTopLine + w.TermTopLine
 }
 
+func NewEmptyDisplayBuffer() *DisplayBuffer {
+	db := &DisplayBuffer{
+		Lines: make([][]byte, 10),
+	}
+	db.AllocateLines(256)
+	return db
+}
+
 func NewWindow(line int, column int, height int, width int) (w *Window) {
 	w = &Window{}
-	w.StartLine = line
+	w.TermTopLine = line
 	w.StartCol = column
 	w.Height = height
 	w.Width = width
-	w.Buf = &DisplayBuffer{}
+	w.EventChan = make(chan *KeyAction, 10)
+	w.Buf = NewEmptyDisplayBuffer()
 	return w
 }
 
 func (w Window) MoveCursorToPosition(line int, col int) {
 	fmt.Fprintf(w.Out, "\x1b[%d;%dH", line, col)
-}
-
-type State struct {
-	Windows      []*Window
-	ActiveWindow *Window
-}
-
-type MainConfig struct {
-	LogConfig *LogConfig
-	In        io.Reader
-	Out       io.Writer
-	State     State
 }
 
 func (w Window) Render(out io.Writer) {
@@ -128,11 +91,24 @@ func (w *Window) MakeNewLines(count int) [][]byte {
 
 func (w *Window) RedrawLine(ln int) {
 	screenLine := ln - w.BufTopLine
-	if screenLine > 0 && screenLine < w.BufTopLine+w.Height {
+	if screenLine >= 0 && screenLine < w.BufTopLine+w.Height {
 		w.MoveCursorToPosition(screenLine+w.TermTopLine, 0)
 		w.Out.Write(TERM_CLEAR_LINE)
 		w.Out.Write(w.Buf.Lines[ln])
 	}
+}
+
+func (w *Window) NewBuffer() {
+	w.Buf = &DisplayBuffer{}
+	w.Buf.Lines = make([][]byte, 1, 100)
+	for i := 0; i < len(w.Buf.Lines); i++ {
+		w.Buf.Lines[i] = make([]byte, 0, 256)
+	}
+	w.Buf.Size = len(w.Buf.Lines)
+	w.Buf.ActiveLine = 0
+	w.CursorLine = 0
+	w.CursorCol = 0
+	w.Redraw(w.MakeRedrawHandler())
 }
 
 func (w *Window) Listen() {
@@ -142,6 +118,7 @@ func (w *Window) Listen() {
 		ka = <-w.EventChan
 		if ka.PrintRaw && len(ka.Value) == 1 {
 			w.WriteRaw(ka.Value)
+			w.KeyActionReturner <- ka
 		} else {
 			switch ka.Action {
 			case "Backspace":
@@ -165,11 +142,13 @@ func (w *Window) Listen() {
 			case "Enter":
 				newLine := w.MakeNewLines(1)
 				w.WriteRaw([]byte("\r\n"))
+				w.IncrCursorLine(1)
+				w.CursorCol = 0
 				w.Buf.Lines = InsertLineAt(w.Buf.Lines, newLine, w.CursorLine)
 				w.Redraw(redrawHandler)
+				w.MoveCursorToPosition(w.CursorLine, w.CursorCol)
 			}
 		}
-		w.KeyActionReturner <- ka
 	}
 }
 
@@ -179,12 +158,16 @@ func (w *Window) Redraw(handler func() []int) {
 	w.MoveCursorToPosition(w.TermTopLine, 0)
 	for _, lineNum := range linesToRedraw {
 		w.MoveCursorToPosition(w.TermTopLine+lineNum, 0)
-		RedrawLine(w.Buf.Lines[lineNum+w.Buf.TopLine])
+		w.RedrawLine(lineNum + w.BufTopLine)
 		lastIndex++
 	}
 }
 
-func (w *Window) MakeRedrawHandler() func() []int {
+func (w *Window) RedrawAllLines() {
+	w.MoveCursorToPosition(w.TermTopLine, 0)
+}
+
+func (w *Window) MakeRedrawHandler() func() []int { // makes a handler that will track indicies to redraw (reuses slice)
 	redrawIndicies := make([]int, 0, w.Height)
 	return func() []int {
 		clear(redrawIndicies)
@@ -234,19 +217,29 @@ func ReturnKeyActionsToPool(p *sync.Pool, returner chan *KeyAction) {
 }
 
 func MainEventHandler(mc *MainConfig) {
+	gl := GlobalLogger
 	var ka *KeyAction
 	fd := int(os.Stdin.Fd())
+	gl.Logln("Setting terminal to raw mode")
+	gl.Logln("")
 	oldState, err := term.MakeRaw(fd)
 	if err != nil {
 		panic(err)
 	}
+	gl.Logln("Making closer channel")
 	closer := make(chan interface{})
-	sp := MakeSequencePool() // Create a pool of *SequenceNode references for dispatching normal ascii printable characters on the event channel for the window (trying to avoid as much re-allocation as possible
+	gl.Logln("Making *KeyAction pool")
+	sp := MakeKeyActionPool() // Create a pool of *SequenceNode references for dispatching normal ascii printable characters on the event channel for the window (trying to avoid as much re-allocation as possible
+	gl.Logln("Making *KeyAction return channel for the pool")
 	keyActionReturner := make(chan *KeyAction, 1000)
+	gl.Logln("Spinning off goroutine for returning *KeyActions to the pool")
 	go ReturnKeyActionsToPool(sp, keyActionReturner)
+	gl.Logln("Spinning off goroutine for restoring the  terminal state")
 	go Cleanup(closer, fd, oldState, mc.LogConfig)
 	buf := make([]byte, 1)
+	gl.Logln("Creating byte handler from closure")
 	byteHandler := MakeByteHandler(closer, mc.In, sp)
+	gl.Logln("Entering main event loop")
 	for {
 		nb, err := mc.In.Read(buf)
 		if err != nil {
@@ -254,11 +247,13 @@ func MainEventHandler(mc *MainConfig) {
 		}
 		if nb > 0 {
 			b := buf[0]
+			gl.Logln("Read byte %x", b)
 			// res = HandleByte(b, closer, mc.In) // this should return the final coerced byte or []byte that the window will be responsible for processing
 			ka = byteHandler(b)
 			if ka == nil {
 				ka = sp.Get().(*KeyAction)
 			}
+			gl.Logln("Generated *KeyAction: %s", ka.String())
 			mc.State.ActiveWindow.EventChan <- ka
 			// mc.State.ActiveWindow.RawEventChan <- res
 		}
@@ -270,7 +265,7 @@ func (mc *MainConfig) CoerceInput(b byte) (inputSeq []byte) { // Will coerce inp
 	return []byte{}
 }
 
-func MakeSequencePool() *sync.Pool {
+func MakeKeyActionPool() *sync.Pool {
 	return &sync.Pool{
 		New: func() any {
 			return NewKeyActionFromPool(true, "Print", true, 0x00)
@@ -288,7 +283,7 @@ func CoerceInputToAction(b []byte) *KeyAction {
 func MakeByteHandler(ch chan interface{}, in io.Reader, sp *sync.Pool) func(byte) *KeyAction { // returns a byte handling function that will reuse an input buffer so re-allocation does not happen on every byte handled by the main loop
 	res := make([]byte, 1, 8)
 	var seqN *KeyAction
-	sp := MakeSequencePool() // Create a pool of *SequenceNode references for dispatching normal ascii printable characters on the event channel for the window (trying to avoid as much re-allocation as possible
+	sp = MakeKeyActionPool() // Create a pool of *SequenceNode references for dispatching normal ascii printable characters on the event channel for the window (trying to avoid as much re-allocation as possible
 	return func(b byte) *KeyAction {
 		if b == 3 {
 			ch <- struct{}{}
@@ -345,4 +340,50 @@ func HandleByte(b byte, ch chan interface{}, in io.Reader) (res []byte) {
 
 	}
 	return res
+}
+
+func (cl *ConcreteLogger) Log(message string, vars ...any) {
+	cl.LogCh <- fmt.Sprintf(message, vars...)
+}
+
+func (cl *ConcreteLogger) Logln(message string, vars ...any) {
+	cl.LogCh <- fmt.Sprintf(message+"\n", vars...)
+}
+
+func (cl *ConcreteLogger) Start() {
+	for {
+		select {
+		case msg := <-cl.LogCh:
+			fmt.Fprintf(cl.Out, msg)
+		case <-cl.RunCh:
+			return
+		}
+	}
+}
+
+func (cl *ConcreteLogger) Stop() {
+	cl.RunCh <- struct{}{}
+}
+
+func (cl *ConcreteLogger) Init() {
+	cl.LogCh = make(chan string, 1000)
+	cl.RunCh = make(chan interface{})
+	f, err := os.CreateTemp("./", ".term-reader-logger.txt.")
+	if err != nil {
+		fmt.Printf("Error opening tmp file: %s\n", err)
+		cl.Out = io.Discard
+	} else {
+		os.Remove("term-reader-logger.txt")
+		err := os.Symlink(f.Name(), "term-reader-logger.txt")
+		if err != nil {
+			fmt.Printf("Error creating logger symlink: %s\n", err)
+		}
+		cl.Out = f
+		cl.Log("Opened New logger at %s", f.Name())
+	}
+}
+
+func NewConcreteLogger(cl *ConcreteLogger) {
+	cl = &ConcreteLogger{}
+	cl.Init()
 }
