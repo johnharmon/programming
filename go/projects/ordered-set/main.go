5d0c0ad1393cdb28d6d0730b1390750e3e0b7c9a
John Harmon <johnharmon2017@gmail.com>
2025-09-08 22:44:24 -0600

    more work on the ordered set, mostly to do some compaction via taking a snapshot and re-generating the element list with only live elements in the background. this will involve later logging all modification operations while compaction is happening and then replaying those into the new element set

--- go/projects/ordered-set/main.go
+++ go/projects/ordered-set/main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"iter"
+	"sync"
 )
 
 type OrderedSet[T comparable] struct {
-	statusBitMap []uint64
-	items        []T
-	indexMap     map[T]int
-	liveElements int
-	tombstones   int
+	bitmap     []uint64
+	items      []T
+	indexMap   map[T]int
+	liveCount  int
+	tombstones int
+	compacting bool
+	rwLock     *sync.RWMutex
 }
 
 func (oSet *OrderedSet[T]) Append(elem T) bool {
 	if _, ok := oSet.indexMap[elem]; !ok {
 		oSet.items = append(oSet.items, elem)
 		oSet.indexMap[elem] = len(oSet.items) - 1
-		oSet.liveElements += 1
+		oSet.liveCount += 1
 		oSet.appendBitMap(oSet.indexMap[elem])
 		return true
 	}
@@ -26,19 +29,43 @@ func (oSet *OrderedSet[T]) Append(elem T) bool {
 func (oSet *OrderedSet[T]) appendBitMap(idx int) {
 	uintIndex := idx / 64
 	bitOffset := idx % 64
-	if uintIndex >= len(oSet.statusBitMap) {
-		oSet.statusBitMap = append(oSet.statusBitMap, uint64(0))
+	if uintIndex >= len(oSet.bitmap) {
+		oSet.bitmap = append(oSet.bitmap, uint64(0))
 	}
 	bitMask := uint64(1) << bitOffset
-	oSet.statusBitMap[uintIndex] |= bitMask
+	oSet.bitmap[uintIndex] |= bitMask
+}
+
+func (oSet *OrderedSet[T]) compact() {
+	oSet.rwLock.RLock()
+	oSet.compacting = true
+	bitmapSnapshot := make([]uint64, len(oSet.bitmap))
+	copy(bitmapSnapshot, oSet.bitmap)
+	newItems := make([]T, len(oSet.items))
+	liveCount := 0
+	for idx := range oSet.items {
+		if isAlive(bitmapSnapshot, idx) {
+			newItems[liveCount] = oSet.items[idx]
+			liveCount++
+		}
+	}
+	oSet.rwLock.RUnlock()
+	newBitmap := make([]uint64, (liveCount+63)/64)
+	bitmapRemainder := liveCount % 64
+	for idx := range newBitmap {
+		newBitmap[idx] = ^uint64(0)
+	}
+	if bitmapRemainder != 0 {
+		newBitmap[len(newBitmap)-1] = (uint64(1) << bitmapRemainder) - 1
+	}
 }
 
 func (oSet *OrderedSet[T]) deleteBitMap(idx int) {
 	uintIndex := idx / 64
 	bitOffset := idx % 64
-	if uintIndex < len(oSet.statusBitMap) {
+	if uintIndex < len(oSet.bitmap) {
 		bitMask := uint64(1) << bitOffset
-		oSet.statusBitMap[uintIndex] &^= bitMask
+		oSet.bitmap[uintIndex] &^= bitMask
 	}
 }
 
@@ -51,11 +78,22 @@ func (oSet *OrderedSet[T]) Delete(elem T) bool {
 	return false
 }
 
-func (oSet *OrderedSet[T]) isAlive(elem T) bool {
+func (oSet *OrderedSet[T]) isValAlive(elem T) bool {
 	if _, ok := oSet.indexMap[elem]; ok {
 		return true
 	}
 	return false
+}
+
+// func getBitMapIndex(idx int) uint64
+func (oSet *OrderedSet[T]) isIdxAlive(idx int) bool {
+	if idx < 0 || idx > len(oSet.bitmap) {
+		return false
+	}
+	uintIndex := idx / 64
+	bitOffset := idx % 64
+	bitMask := uint64(1) << bitOffset
+	return (oSet.bitmap[uintIndex] & bitMask) != 0
 }
 
 func (oSet *OrderedSet[T]) In(elem T) bool {
@@ -69,7 +107,7 @@ func (oSet *OrderedSet[T]) In(elem T) bool {
 func (oSet *OrderedSet[T]) Iter() iter.Seq[T] {
 	return func(yield func(T) bool) {
 		for _, item := range oSet.items {
-			if oSet.isAlive(item) {
+			if oSet.isValAlive(item) {
 				if !(yield(item)) {
 					return
 				}
@@ -82,7 +120,7 @@ func (oSet *OrderedSet[T]) IterIndex() iter.Seq2[int, T] {
 	return func(yield func(int, T) bool) {
 		liveIdx := 0
 		for _, item := range oSet.items {
-			if oSet.isAlive(item) {
+			if oSet.isValAlive(item) {
 				if !(yield(liveIdx, item)) {
 					return
 				}
@@ -90,4 +128,14 @@ func (oSet *OrderedSet[T]) IterIndex() iter.Seq2[int, T] {
 			}
 		}
 	}
+}
+
+func isAlive(bitmap []uint64, idx int) bool {
+	if idx < 0 || idx > len(bitmap) {
+		return false
+	}
+	uintIndex := idx / 64
+	bitOffset := idx % 64
+	bitMask := uint64(1) << bitOffset
+	return (bitmap[uintIndex] & bitMask) != 0
 }
