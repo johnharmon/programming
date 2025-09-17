cf79e1cd29a39e0921ef8d71b8bba4edb5491f51
John Harmon <johnharmon2017@gmail.com>
2025-09-17 16:06:45 -0600

    splitting types into their own file :)

---
+++ go/projects/ordered-set/types.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"io"
+	"sync"
+	"sync/atomic"
+	"reflect"
+)
+
+type setOp[T comparable] struct {
+	callback chan bool
+	opVal    *T
+	opIdx    int
+	opType   int
+	seqNo    uint64
+}
+
+type setMember[T comparable] struct {
+	bitmapIdx int
+	Value     *T
+}
+
+type setEntity[T comparable] struct {
+	Value T
+	ID    uint64
+}
+
+type replayOp[T comparable] struct {
+	opType int
+	seqNo  uint64
+	value  *T
+	opIdx  int
+}
+
+type OrderedSet[T comparable] struct {
+	dualWrite       bool
+	bitmap          []uint64
+	compacting      bool
+	cBitmap         []uint64
+	cItems          []uint64
+	cDataToSequence map[T]uint64
+	dataToSequence  map[T]uint64
+	sequenceToData  map[uint64]setMember[T]
+	cSequenceToData map[uint64]setMember[T]
+	snapshotSeqNo   uint64
+	opPool          *sync.Pool
+	items           []uint64
+	indexMap        map[T]int
+	liveCount       atomic.Uint64
+	cLiveCount      int
+	seqNo           atomic.Uint64 // integer representing the most recent operation number so ops can be tagged with it
+	lastAppliedSeq  uint64
+	cLastAppliedSeq uint64
+	rwLock          *sync.RWMutex
+	pending         map[uint64]setOp[T]
+	opsCh           chan *setOp[T]
+	replayCh        chan replayOp[T]
+	tombstones      int
+	maxItems        int
+	cAppendBitmap   func(int)
+	appendBitmap    func(int)
+	// cAppend func(
+}
+
+type setOpEncoder[T comparable] struct {
+	writer io.Writer
+}
+
+func (s *setOpEncoder[T]) Encode(op setOp[T]) {
+	s.writer.Write("Type: %s, Value: %s, Idx: %d, opType: %s, seqNo: %d\n" op.opVal)
+}
