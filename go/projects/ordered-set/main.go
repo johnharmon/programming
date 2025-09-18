402eb1a3df2470c2dcc00546c370a3b0e95f61d6
John Harmon <placeholder>
2025-09-18 03:17:29 -0600

    starting testing, fixing up some internals, fixed getNthAliveItem, etc

--- go/projects/ordered-set/main.go
+++ go/projects/ordered-set/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 
 	"harmonlab.io/set"
 )
@@ -15,5 +17,33 @@ func main() {
 	for i := 0; i < 5; i++ {
 		res := mySet.Append(i)
 		fmt.Printf("Change Applied: %t\n", res)
+	}
+	//	fmt.Println("Deleting items:")
+	//	for i := 0; i < 5; i++ {
+	//		res := mySet.Delete(i)
+	//		fmt.Printf("Change Applied: %t\n", res)
+	//	}
+	//	fmt.Println("Deleting items:")
+	//	for i := 0; i < 5; i++ {
+	//		res := mySet.Delete(i)
+	//		fmt.Printf("Change Applied: %t\n", res)
+	//	}
+	for i := 0; i < 5; i++ {
+		res := mySet.Append(rand.Int())
+		fmt.Printf("Change Applied: %t\n", res)
+	}
+	mySet.DumpBitMap(os.Stdout)
+	mySet.DumpItems(os.Stdout)
+	mySet.DumpSequenceMap(os.Stdout)
+	mySet.DumpDataMap(os.Stdout)
+	for i := 0; i < 5; i++ {
+		setLen := mySet.Len()
+		idx := rand.Int() % setLen
+		if res, ok := mySet.GetIdx(idx); ok {
+			fmt.Printf("Value: %d, index: %d\n", res.Value, idx)
+		} else {
+			fmt.Printf("Error: index(%d) not found in set\n", idx)
+		}
+
 	}
 }
